16ed91a75b94c82d968d48026c320a145a33c858
srinadh1304 <srinadh1304@gmail.com>
2021-08-24 17:45:49 +0530

    ADDED:Player plays till he wins or loses 50%

--- src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
+++ src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
@@ -1,28 +1,40 @@
 package com.bridgelabz.gamblersimulation;
 
-public class GamblerSimulation
+public class GamblerSimulation 
 {
+
 	public static final int TOTAL_STAKE=100;
 	public static final int BET_PER_GAME=1;
 
-	public static int playGame(int availableBalance) {
+	public static int playGame(int availableBalance)
+	{
 		double wonOrLost=Math.random();
-		if(wonOrLost>0.5) {
-			System.out.println("Won the game");
+		if(wonOrLost>0.5)
+		{
 			return availableBalance+BET_PER_GAME;
 		}
-		else {
-			System.out.println("Lost the game");
+		else 
+		{
 			return availableBalance-BET_PER_GAME;
 		}
 	}
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)
+	{
+
 		System.out.println("Total stake="+TOTAL_STAKE);
 		System.out.println("Bet per game="+BET_PER_GAME);
 		int availableBalance=TOTAL_STAKE;
-		availableBalance=playGame(availableBalance);
-		System.out.println(availableBalance);
+		while(availableBalance<=1.5*TOTAL_STAKE && availableBalance>=0.5*TOTAL_STAKE) 
+		{
+			availableBalance=playGame(availableBalance);
+		}
+		if(availableBalance>1.5*TOTAL_STAKE)
+			System.out.println("Player resigned by winning 50%");
+		else
+			System.out.println("Player resigned by losing 50%");
+			
+		System.out.println("Balance available:"+availableBalance);
 
 	}
+
 }

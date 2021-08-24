47985d5fcd2161da3bb0ba92c8c5d9d671b17b3e
srinadh1304 <srinadh1304@gmail.com>
2021-08-24 17:36:28 +0530

    ADDED:Method to win or lose

--- src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
+++ src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
@@ -5,9 +5,24 @@ public class GamblerSimulation
 	public static final int TOTAL_STAKE=100;
 	public static final int BET_PER_GAME=1;
 
-	public static void main(String[] args)
-	{
-		System.out.println("Welcome to gambler Simulation");
+	public static int playGame(int availableBalance) {
+		double wonOrLost=Math.random();
+		if(wonOrLost>0.5) {
+			System.out.println("Won the game");
+			return availableBalance+BET_PER_GAME;
+		}
+		else {
+			System.out.println("Lost the game");
+			return availableBalance-BET_PER_GAME;
+		}
+	}
+	public static void main(String[] args) {
+		
+		System.out.println("Total stake="+TOTAL_STAKE);
+		System.out.println("Bet per game="+BET_PER_GAME);
+		int availableBalance=TOTAL_STAKE;
+		availableBalance=playGame(availableBalance);
+		System.out.println(availableBalance);
 
 	}
 }

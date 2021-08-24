4ba7290141c2bd4abc06eb5f7f30269e3cf1f82d
srinadh1304 <srinadh1304@gmail.com>
2021-08-24 19:11:51 +0530

    ADDED: Plays next month if won or stops

--- src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
+++ src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
@@ -6,7 +6,7 @@ public class GamblerSimulation
 	public static final int TOTAL_STAKE=100;
 	public static final int BET_PER_GAME=1;
 
-	
+
 	public static void main(String[] args)
 	{
 		int availableBalance=TOTAL_STAKE;
@@ -35,22 +35,25 @@ public class GamblerSimulation
 	{
 		int noOfDays=30;
 		int amountInADay=0;
+		int totalAmount=0;
 		for(int day=1; day<=noOfDays;day++) 
 		{
+			System.out.println();
 			System.out.print("Day "+day);
 			amountInADay=gameResign(TOTAL_STAKE);
+			totalAmount+=amountInADay; 
 			if(amountInADay==TOTAL_STAKE+0.5*TOTAL_STAKE) 
 			{
-				System.out.println(" Won By:"+(int)(TOTAL_STAKE+0.5*TOTAL_STAKE-TOTAL_STAKE));
+				System.out.println(" Won By:"+(int)(TOTAL_STAKE*1.5));
 			}
 			else 
 			{
-				System.out.println(" Lost By:"+(int)(TOTAL_STAKE-0.5*TOTAL_STAKE));
+				System.out.println(" Lost By:"+(int)(0.5*TOTAL_STAKE));
 			}
 
 
 		}
-
+		playNextMonth(totalAmount);
 
 	}
 	public static int gameResign(int availableBalance) 
@@ -64,21 +67,25 @@ public class GamblerSimulation
 		return availableBalance;
 
 	}
-	public static void luckyAndUnluckyDay() {
+	public static void luckyAndUnluckyDay() 
+	{
 		int noOfDays=30;
 		int amountInADay=0;
 		String luckyDays="";
 		String unLuckyDays="";
-		for(int day=1; day<=noOfDays;day++) {
+		for(int day=1; day<=noOfDays;day++) 
+		{
 			amountInADay=gameResign(TOTAL_STAKE);
-			if(amountInADay==TOTAL_STAKE+0.5*TOTAL_STAKE) {
+			if(amountInADay==TOTAL_STAKE+0.5*TOTAL_STAKE)
+			{
 				luckyDays+=day+" ";
 			}
-			else {
+			else 
+			{
 				unLuckyDays+=day+" ";
 			}
-				
-			
+
+
 		}
 		System.out.println("Lucky days:"+luckyDays);
 		System.out.println("Unlucky days:"+unLuckyDays);
@@ -93,6 +100,19 @@ public class GamblerSimulation
 			totalAmount+=amountInOneDay;
 		}
 		return totalAmount;
+
+	}
+	public static void playNextMonth(int totalAmount) 
+	{
+
+		if( totalAmount >= (TOTAL_STAKE*30) )
+		{
+			System.out.print("Plays Next Month also");
+			gameForMonth();
+			
+		}
+		else
+			System.out.println("Does not play next month");
 
 	}
 
